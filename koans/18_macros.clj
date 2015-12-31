306b30808352b6483f119eb1ef3e95d2c2f9f459
Saeed Gatson <saeedgatson@gmail.com>
2015-12-30 23:11:47 -0800

    Add Answers For Koan 18 Macros

--- koans/18_macros.clj
+++ koans/18_macros.clj
@@ -9,37 +9,37 @@
 
 (defmacro infix-better [form]
   `(~(second form) ; Note the syntax-quote (`) and unquote (~) characters!
-    __
-    __ ))
+    ~(first form)
+    ~(nth form 2)))
 
 (defmacro r-infix [form]
   (cond (not (seq? form))
-        __
+        form
         (= 1 (count form))
         `(r-infix ~(first form))
         :else
         (let [operator (second form)
               first-arg (first form)
-              others __]
+              others (rest (rest form))]
           `(~operator
             (r-infix ~first-arg)
             (r-infix ~others)))))
 
 (meditations
   "Macros are like functions created at compile time"
-  (= __ (hello "Macros!"))
+  (= "Hello, Macros!" (hello "Macros!"))
 
   "I can haz infix?"
-  (= __ (infix (9 + 1)))
+  (= 10 (infix (9 + 1)))
 
   "Remember, these are nothing but code transformations"
-  (= __ (macroexpand '(infix (9 + 1))))
+  (= '(+ 9 1) (macroexpand '(infix (9 + 1))))
 
   "You can do better than that - hand crafting FTW!"
-  (= __ (macroexpand '(infix-better (10 * 2))))
+  (= '(* 10 2) (macroexpand '(infix-better (10 * 2))))
 
   "Things don't always work as you would like them to... "
-  (= __ (macroexpand '(infix-better ( 10 + (2 * 3)))))
+  (= '(+ 10 (2 * 3)) (macroexpand '(infix-better ( 10 + (2 * 3)))))
 
   "Really, you don't understand recursion until you understand recursion"
   (= 36 (r-infix (10 + (2 * 3) + (4 * 5)))))
